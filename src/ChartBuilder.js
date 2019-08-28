24ff4dc5c58e84603406cceb5dac0cd101c09b6b
Sagar Ghimire <info@sagarg.com.np>
2019-08-28 15:35:27 +0545

    control aligned and reduced font size (#3)
    * control aligned and reduced font size
    
    * Consistent Style on controls
    
    * build updated
    
    * fixed left align reatively

--- src/ChartBuilder.js
+++ src/ChartBuilder.js
@@ -30,19 +30,17 @@ export const ChartBuilder = (props) => {
   }
 
   return (
-    <div className="text-center w-full max-w-lg">
+    <div className="">
       <Formik
         initialValues={{ xAxis: fields[0].name, yAxis: [fields[0].name], chartType: chartTypes[0] }}
         onSubmit={values => handleSubmit(values)}
         render={({ values, setFieldValue }) => (
-          <Form className="bg-white">
-
-            <div className="flex flex-wrap -mx-3 mb-2">
-
-              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label htmlFor="chartType" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Chart type</label>
+          <Form className="bg-white bg-white text-sm p-3 text-left">
+            <div className="flex flex-wrap ">
+              <div className="w-full mb-3">
+                <label htmlFor="chartType" className="text-xs font-bold uppercase text-gray-700">Chart type</label>
                 <div className="relative">
-                  <Field name="chartType" component="select" placeholder="Chart type" required className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                  <Field name="chartType" component="select" placeholder="Chart type" required className="block appearance-none w-full mt-1 bg-gray-200 border border-gray-200 text-gray-700 py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
                     {chartTypes.map((type, index) => {
                       return <option value={type} key={`chartType${index}`}>{type}</option>
                     })}
@@ -53,10 +51,10 @@ export const ChartBuilder = (props) => {
                 </div>
               </div>
 
-              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label htmlFor="xAxis" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Group column</label>
+              <div className="w-full mb-3">
+                <label htmlFor="xAxis" className="text-xs font-bold uppercase text-gray-700">Group column</label>
                 <div className="relative">
-                  <Field name="xAxis" component="select" placeholder="Field for X axis" required className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                  <Field name="xAxis" component="select" placeholder="Field for X axis" required className="block appearance-none w-full mt-1 bg-gray-200 border border-gray-200 text-gray-700 py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
                     {fields.map((field, index) => {
                       return <option value={field.name} key={`field${index}`}>{field.name}</option>
                     })}
@@ -67,8 +65,8 @@ export const ChartBuilder = (props) => {
                 </div>
               </div>
 
-              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label htmlFor="yAxis" className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Series</label>
+              <div className="w-full mb-3">
+                <label htmlFor="yAxis" className="text-xs font-bold uppercase text-gray-700">Series</label>
                 <Field name="yAxis" placeholder="Field for Y axis" className="mb-4 mr-4">
                   {({ field, form }) => (
                     fields.map((field, index) => {
@@ -104,18 +102,17 @@ export const ChartBuilder = (props) => {
 
             </div>
 
-            <div className="flex items-center">
-              <div className="w-1/3"></div>
-              <div className="w-1/3">
+            <div className="w-full mt-2 mb-3">
+              <div className="flex justify-center">
                 <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                   Add view
                 </button>
               </div>
-              <div className="md:w-1/3"></div>
             </div>
           </Form>
+          
         )}
       />
-    </div>
+      </div>
   );
 }
